16f28f72fce697e04f4f8f78992a140ccb350a24
Dave <dave.sabol@ovyl.io>
2025-10-22 14:12:40 -0500

    Add logging module

---
+++ logging/include/ovyl/flash_log_backend.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 Ovyl
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file flash_log_backend.h
+ * @brief Interface for forcing inclusion of the flash log backend.
+ */
+
+#ifndef OVYL_FLASH_LOG_BACKEND_H
+#define OVYL_FLASH_LOG_BACKEND_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* OVYL_FLASH_LOG_BACKEND_H */
